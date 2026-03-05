279e2932778e77dcecc638f9b2b019d6a856700c
mingij <jeffawesome.ja@gmail.com>
2026-03-05 11:11:54 +0100

    new neovim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,21 +1,67 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.clipboard:append("unnamedplus")
-vim.opt.rtp:prepend(lazypath)
-vim.o.number = true
-require("vim-options")
-vim.opt.termguicolors = true
-require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-})
+require("config.lazy")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+vim.keymap.set({ "n", "t" }, "<space>tt", "<cmd>FloatermToggle<CR>")
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+local job_id = 0
+vim.keymap.set("n", "<space>to", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 5)
+
+  job_id = vim.bo.channel
+end)
+
+local current_command = ""
+
+-- keymaps
+
+vim.keymap.set("n", "<space>te", function()
+  current_command = vim.fn.input("Command: ")
+end)
+
+vim.keymap.set("n", "<space>tr", function()
+  if current_command == "" then
+    current_command = vim.fn.input("Command: ")
+  end
+
+  vim.fn.chansend(job_id, { current_command .. "\r\n" })
+end)
+vim.keymap.set("n", "-", "<cmd>Oil<CR>")
+
+vim.keymap.set({ "x", "o" }, "af", function()
+  require("nvim-treesitter-textobjects.select")
+      .select_textobject("@function.outer", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "if", function()
+  require("nvim-treesitter-textobjects.select")
+      .select_textobject("@function.inner", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "ac", function()
+  require("nvim-treesitter-textobjects.select")
+      .select_textobject("@class.outer", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "ic", function()
+  require("nvim-treesitter-textobjects.select")
+      .select_textobject("@class.inner", "textobjects")
+end)
+
+vim.keymap.set({ "n", "x", "o" }, "]m", function()
+  require("nvim-treesitter-textobjects.move")
+      .goto_next_start("@function.outer", "textobjects")
+end)
+
+vim.keymap.set('n', '<C-l>', function()
+  vim.cmd('nohlsearch')
+  vim.cmd('normal! <C-l>')
+end, { silent = true })
